64b0c96010dae834e1f0f9d9f1301f97b95fad3d
Trent Brew <trentbrew@Trents-MacBook-Pro.local>
2021-06-16 21:37:48 -0500

    initial

---
+++ src/components/EditableText.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="editable-text"><div class="icon editable-text-icon"></div><p>{{ thisText }}</p></div>
+</template>
+
+<script>
+export default {
+    name: 'EditableText',
+    data() {
+        return {
+            thisText: null
+        }
+    },
+    props: {
+        rel: String
+    },
+    mounted() {
+        this.fetchText();
+    },
+    methods: {
+        fetchText() {
+            this.fireFetch("text", "rel == $rel", (docs) => {
+                docs.forEach((doc) => {
+                    console.log(doc.data());
+                    this.thisText = doc.data().value;
+                    console.log(this.thisText);
+                });
+            });
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/styles/global'
+
+
+
+</style>
